c20645f2732c2d70de77fc1f79660a44637dccf8
ilumsden <lumsden.ian@gmail.com>
2023-02-08 12:10:42 -0500

    docs: fixes some mistakes in publications.rst

--- docs/demos/ecp_feb_2023/cons.c
+++ docs/demos/ecp_feb_2023/cons.c
@@ -17,85 +17,79 @@
  *
  * @return 0 if no errors occured. -1 otherwise.
  */
-int consume_file(char* full_path, int32_t seed, int32_t* val_buf)
+int consume_file (char* full_path, int32_t seed, int32_t* val_buf)
 {
     // Open the file or abort if not possible
-    FILE* fp = fopen(full_path, "r");
+    FILE* fp = fopen (full_path, "r");
     // Print an error if the file couldn't be opened
-    if (fp == NULL)
-    {
-        fprintf(stderr, "Cannot open file: %s\n", full_path);
-        printf("File %ld: Cannot open\n", (long) seed);
+    if (fp == NULL) {
+        fprintf (stderr, "Cannot open file: %s\n", full_path);
+        printf ("File %ld: Cannot open\n", (long)seed);
         return -1;
     }
     // Read the file or abort if not possible
-    size_t items_read = fread(val_buf, sizeof(int32_t), NUM_VALS, fp);
-    fclose(fp);
+    size_t items_read = fread (val_buf, sizeof (int32_t), NUM_VALS, fp);
+    fclose (fp);
     // Print an error if the number of items read is not what was expected
-    if (items_read != NUM_VALS)
-    {
-        fprintf(stderr, "Could not read the full file (%s)\n", full_path);
-        printf("File %ld: Cannot read\n", (long) seed);
+    if (items_read != NUM_VALS) {
+        fprintf (stderr, "Could not read the full file (%s)\n", full_path);
+        printf ("File %ld: Cannot read\n", (long)seed);
         return -1;
     }
     return 0;
 }
 
-int main(int argc, char** argv)
+int main (int argc, char** argv)
 {
     // Parse command-line arguments
     int32_t num_transfers;
     char* fpath;
-    int rc = parse_cmd_line(argc, argv, &num_transfers, &fpath);
+    int rc = parse_cmd_line (argc, argv, &num_transfers, &fpath);
     // If an error occured during command-line parsing,
     // abort the consumer
-    if (rc != 0)
-    {
+    if (rc != 0) {
         return rc;
     }
     // Largest number of digits for a int32_t when converted to string
     const size_t max_digits = 10;
     // First 4 is for "data"
     // Second 4 is for ".txt"
-    size_t path_len = strlen(fpath) + 4 + max_digits + 4;
+    size_t path_len = strlen (fpath) + 4 + max_digits + 4;
     // Allocate a buffer for the file path
-    char* full_path = malloc(path_len + 1);
+    char* full_path = malloc (path_len + 1);
     // Allocate a buffer for the data read from the file
-    int32_t* val_buf = ALLOC_VAL_BUF();
+    int32_t* val_buf = ALLOC_VAL_BUF ();
     int num_chars;
-    for (int32_t seed = 0; seed < num_transfers; seed++)
-    {
+    for (int32_t seed = 0; seed < num_transfers; seed++) {
         // Clear full_path and val_buf to be safe
-        memset(full_path, '\0', path_len+1);
-        memset(val_buf, 0, VAL_BUF_SIZE);
+        memset (full_path, '\0', path_len + 1);
+        memset (val_buf, 0, VAL_BUF_SIZE);
         // Generate the file name
-        num_chars = snprintf(full_path, path_len+1, "%s/data%ld.txt", fpath, (long) seed);
+        num_chars =
+            snprintf (full_path, path_len + 1, "%s/data%ld.txt", fpath, (long)seed);
         // If the file name could not be generated, print an error
         // and abort
-        if (num_chars < 0)
-        {
-            fprintf(stderr, "Could not generate file name for transfer %ld\n", (long) seed);
-            printf("File %ld: Bad Name\n", (long) seed);
+        if (num_chars < 0) {
+            fprintf (stderr,
+                     "Could not generate file name for transfer %ld\n",
+                     (long)seed);
+            printf ("File %ld: Bad Name\n", (long)seed);
             continue;
         }
         /********************************************
          *       Perform DYAD Data Transfer!!!      *
          ********************************************/
-        rc = consume_file(full_path, seed, val_buf);
-        if (rc != 0)
-        {
+        rc = consume_file (full_path, seed, val_buf);
+        if (rc != 0) {
             continue;
         }
         // Validate the content of the file
-        if (vals_are_valid(seed, val_buf))
-        {
-            printf("File %ld: OK\n", (long) seed);
-        }
-        else
-        {
-            printf("File %ld: BAD\n", (long) seed);
+        if (vals_are_valid (seed, val_buf)) {
+            printf ("File %ld: OK\n", (long)seed);
+        } else {
+            printf ("File %ld: BAD\n", (long)seed);
         }
     }
-    free(val_buf);
-    free(full_path);
+    free (val_buf);
+    free (full_path);
 }
--- docs/demos/ecp_feb_2023/prod.cpp
+++ docs/demos/ecp_feb_2023/prod.cpp
@@ -1,12 +1,11 @@
-#include "generation.h"
-
 #include <sys/stat.h>
 
+#include <dyad_stream_api.hpp>
 #include <fstream>
 #include <iostream>
 #include <string>
 
-#include <dyad_stream_api.hpp>
+#include "generation.h"
 
 /**
  * Write the spcified file and record it in for transfer with DYAD
@@ -18,49 +17,42 @@
  *
  * @return 0 if no errors occured. -1 otherwise.
  */
-int produce_file(std::string& full_path, int32_t seed, int32_t* val_buf)
+int produce_file (std::string& full_path, int32_t seed, int32_t* val_buf)
 {
     // Open the file or abort if not possible
     dyad::ofstream_dyad ofs_dyad;
-    try
-    {
-        ofs_dyad.open(full_path, std::ofstream::out | std::ios::binary);
-    }
-    catch (std::ios_base::failure)
-    {
+    try {
+        ofs_dyad.open (full_path, std::ofstream::out | std::ios::binary);
+    } catch (std::ios_base::failure) {
         std::cerr << "Cannot open file: " << full_path << "\n";
         std::cout << "File " << seed << ": Cannot open\n";
         return -1;
     }
     // Get the C++ std::ofstream from the DYAD stream
-    std::ofstream& ofs = ofs_dyad.get_stream();
+    std::ofstream& ofs = ofs_dyad.get_stream ();
     // Write the file or abort if not possible
-    try
-    {
-        ofs.write((char*) val_buf, VAL_BUF_SIZE);
-    }
-    catch (std::ios_base::failure)
-    {
-        ofs_dyad.close();
+    try {
+        ofs.write ((char*)val_buf, VAL_BUF_SIZE);
+    } catch (std::ios_base::failure) {
+        ofs_dyad.close ();
         std::cerr << "Could not write the full file (" << full_path << ")\n";
         std::cout << "File " << seed << ": Cannot write\n";
         return -1;
     }
     // If everything went well, close the file
-    ofs_dyad.close();
+    ofs_dyad.close ();
     return 0;
 }
 
-int main(int argc, char** argv)
+int main (int argc, char** argv)
 {
     // Parse command-line arguments
     int32_t num_transfers;
     char* fpath;
-    int rc = parse_cmd_line(argc, argv, &num_transfers, &fpath);
+    int rc = parse_cmd_line (argc, argv, &num_transfers, &fpath);
     // If an error occured during command-line parsing,
     // abort the consumer
-    if (rc != 0)
-    {
+    if (rc != 0) {
         return rc;
     }
     // Convert the managed directory path to a C++ string
@@ -68,19 +60,17 @@ int main(int argc, char** argv)
     std::string full_path;
     int num_chars;
     size_t items_read;
-    for (int32_t seed = 0; seed < num_transfers; seed++)
-    {
+    for (int32_t seed = 0; seed < num_transfers; seed++) {
         // Generate the file's data
-        int32_t* val_buf = generate_vals(seed);
+        int32_t* val_buf = generate_vals (seed);
         // Generate the file name
-        full_path = cpp_fpath + "/data" + std::to_string(seed) + ".txt";
+        full_path = cpp_fpath + "/data" + std::to_string (seed) + ".txt";
         /********************************************
          *     Perform DYAD Data Production!!!      *
          ********************************************/
-        rc = produce_file(full_path, seed, val_buf);
-        free(val_buf);
-        if (rc != 0)
-        {
+        rc = produce_file (full_path, seed, val_buf);
+        free (val_buf);
+        if (rc != 0) {
             continue;
         }
     }
