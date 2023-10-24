4da69d366bec91b369393664e9ffe56894241d55
Ian Lumsden <lumsden.ian@gmail.com>
2023-10-24 13:55:02 -0400

    Updates CI to run the Python test too

--- .github/prod-cons/dyad_consumer.sh
+++ .github/prod-cons/dyad_consumer.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+this_script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+source $this_script_dir/prod_cons_argparse.sh
+
 export DYAD_PATH_CONSUMER=${DYAD_PATH}_consumer
 mkdir -p ${DYAD_PATH_CONSUMER}
 
-echo "Loading DYAD module"
-
-#flux module load ${DYAD_INSTALL_PREFIX}/lib/dyad.so  $DYAD_PATH_CONSUMER $DYAD_DTL_MODE
-${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/cpp_cons 10 $DYAD_PATH_CONSUMER
+if [[ "$mode" == "${valid_modes[0]}" ]]; then
+    LD_PRELOAD=${DYAD_INSTALL_PREFIX}/lib/dyad_wrapper.so ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/c_cons 10 $DYAD_PATH_CONSUMER
+elif [[ "$mode" == "${valid_modes[1]}" ]]; then
+    ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/cpp_cons 10 $DYAD_PATH_CONSUMER
+elif [[ "$mode" == "${valid_modes[2]}" ]]; then
+    python3 ${GITHUB_WORKSPACE}/tests/pydyad_spsc/consumer.py $DYAD_PATH_CONSUMER 10 50
+else
+    echo "Invalid test mode: $mode"
+    exit 1
+fi
--- tests/pydyad_spsc/run.sh
+++ tests/pydyad_spsc/run.sh
@@ -3,59 +3,74 @@
 # FLUX: --output=pydyad_spsc_test.out
 # FLUX: --error=pydyad_spsc_test.err
 
-DYAD_INSTALL_LIBDIR="/g/g90/lumsden1/ws/insitu_benchmark/dyad/_test_install/lib"
-KVS_NAMESPACE="pydyad_test"
-CONS_MANAGED_PATH="/l/ssd/lumsden1/pydyad_test"
-PROD_MANAGED_PATH="/l/ssd/lumsden1/pydyad_test"
-NUM_FILES=50
-NUM_INTS=500
+if [ -z "${DYAD_INSTALL_LIBDIR}" ]; then
+    echo "DYAD_INSTALL_LIBDIR must be defined"
+    exit 1
+fi
+if [ ! -d "${DYAD_INSTALL_LIBDIR}" ]; then
+    echo "DYAD_INSTALL_LIBDIR ($DYAD_INSTALL_LIBDIR) does not exist"
+    exit 1
+fi
+if [ ! -f "${DYAD_INSTALL_LIBDIR}/dyad.so" ]; then
+    echo "Invalid contents in DYAD_INSTALL_LIBDIR ($DYAD_INSTALL_LIBDIR)"
+    exit 1
+fi
+if [ -z "${DYAD_PATH_CONSUMER}" ]; then
+    if [ -z "${DYAD_PATH}" ]; then
+        echo "Either DYAD_PATH_CONSUMER or DYAD_PATH must be defined"
+        exit 1
+    else
+        DYAD_PATH_CONSUMER="${DYAD_PATH}"
+    fi
+fi
+if [ -z "${DYAD_PATH_PRODUCER}" ]; then
+    if [ -z "${DYAD_PATH}" ]; then
+        echo "Either DYAD_PATH_PRODUCER or DYAD_PATH must be defined"
+        exit 1
+    else
+        DYAD_PATH_PRODUCER="${DYAD_PATH}"
+    fi
+fi
+if [ -z "${DYAD_DTL_MODE}" ]; then
+    DYAD_DTL_MODE="FLUX_RPC"
+fi
+if [ -z "${DYAD_KVS_NAMESPACE}" ]; then
+    DYAD_KVS_NAMESPACE="pydyad_test"
+fi
+if [ -z "${NUM_FILES}" ]; then
+    NUM_FILES=50
+fi
+if [ -z "${NUM_INTS}" ]; then
+    NUM_INTS=50
+fi
 
 export LD_LIBRARY_PATH="$DYAD_INSTALL_LIBDIR:$LD_LIBRARY_PATH"
 
-if [ ! -d "$CONS_MANAGED_PATH" ]; then
-    mkdir -p $CONS_MANAGED_PATH
-fi
-if [ ! -d "$PROD_MANAGED_PATH" ]; then
-    mkdir -p $PROD_MANAGED_PATH
-fi
-
-cat > ./env_file.cons << EOM
-DYAD_PATH_CONSUMER=$CONS_MANAGED_PATH
-DYAD_KVS_NAMESPACE=$KVS_NAMESPACE
-DYAD_DTL_MODE=UCX
-EOM
-
-cat > ./env_file.prod << EOM
-DYAD_PATH_PRODUCER=$PROD_MANAGED_PATH
-DYAD_KVS_NAMESPACE=$KVS_NAMESPACE
-DYAD_DTL_MODE=UCX
-EOM
-
 flux kvs namespace create $KVS_NAMESPACE
 flux exec -r all flux module load $DYAD_INSTALL_LIBDIR/dyad.so \
-    $PROD_MANAGED_PATH UCX
+    $DYAD_PATH_PRODUCER $DYAD_DTL_MODE
 
 flux submit -N 1 --tasks-per-node=1 --exclusive \
     --output="pydyad_cons.out" --error="pydyad_cons.err" \
-    --env-file=./env_file.cons \
+    --env=DYAD_PATH_CONSUMER=$DYAD_PATH_CONSUMER --env=DYAD_DTL_MODE=$DYAD_DTL_MODE --env=DYAD_KVS_NAMESPACE=$KVS_NAMESPACE \
     --flags=waitable \
-    python3 consumer.py $CONS_MANAGED_PATH $NUM_FILES $NUM_INTS
+    python3 consumer.py $DYAD_PATH_CONSUMER $NUM_FILES $NUM_INTS
 flux submit -N 1 --tasks-per-node=1 --exclusive \
     --output="pydyad_prod.out" --error="pydyad_prod.err" \
-    --env-file=./env_file.prod \
+    --env=DYAD_PATH_PRODUCER=$DYAD_PATH_PRODUCER --env=DYAD_DTL_MODE=$DYAD_DTL_MODE --env=DYAD_KVS_NAMESPACE=$KVS_NAMESPACE \
     --flags=waitable \
-    python3 producer.py $PROD_MANAGED_PATH $NUM_FILES $NUM_INTS
+    python3 producer.py $DYAD_PATH_PRODUCER $NUM_FILES $NUM_INTS
     
 flux job wait --all
 
-flux exec -r all flux module unload dyad
+flux exec -r all flux module remove dyad
 flux kvs namespace remove $KVS_NAMESPACE
 
-if [ -d "$CONS_MANAGED_PATH" ]; then
-    rm -r $CONS_MANAGED_PATH
+if [ -d "$DYAD_PATH_CONSUMER" ]; then
+    rm -r $DYAD_PATH_CONSUMER
 fi
-if [ -d "$PROD_MANAGED_PATH" ]; then
-    rm -r $PROD_MANAGED_PATH
+if [ -d "$DYAD_PATH_PRODUCER" ]; then
+    rm -r $DYAD_PATH_PRODUCER
 fi
 
 if [ $? -ne 0 ]; then
