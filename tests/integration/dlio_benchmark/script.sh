8fb1ff9355abc47e66171ee776daa6d15d18e28d
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-11-07 13:21:47 -0800

    added initial integration between dyad and dlio_benchmark

---
+++ tests/integration/dlio_benchmark/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+
+DLIO_ENV=$1
+DYAD_PROJECT_DIR=$2
+DLIO_ENV=/home/haridev/projects/dyad/venv
+DYAD_PROJECT_DIR=/home/haridev/projects/dyad
+SPACK_ENV=/home/haridev/projects/dyad/spack-env/.spack-env/view
+
+source ${DLIO_ENV}/bin/activate
+export PYTHONPATH=${DYAD_PROJECT_DIR}/tests/integration/dlio_benchmark:$PYTHONPATH
+export LD_LIBRARY_PATH=${DLIO_ENV}/lib:${DLIO_ENV}/lib64:${SPACK_ENV}/lib:${SPACK_ENV}/lib64:${LD_LIBRARY_PATH}
+
+echo Generate Data
+dlio_benchmark --config-dir=${DYAD_PROJECT_DIR}/tests/integration/dlio_benchmark/configs workload=dyad_unet3d ++workload.workflow.generate_data=True ++workload.workflow.train=False
+
+echo Run Training
+dlio_benchmark --config-dir=${DYAD_PROJECT_DIR}/tests/integration/dlio_benchmark/configs workload=dyad_unet3d ++workload.workflow.generate_data=False ++workload.workflow.train=True
