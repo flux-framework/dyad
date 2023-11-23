06da6ea1303da316e59e553fd829ae5e57b14bae
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-11-22 20:38:43 -0800

    fix ci

--- .github/prod-cons/dyad_consumer.sh
+++ .github/prod-cons/dyad_consumer.sh
@@ -8,12 +8,12 @@ export DYAD_PATH_CONSUMER=${DYAD_PATH}_consumer
 mkdir -p ${DYAD_PATH_CONSUMER}
 
 if [[ "$mode" == "${valid_modes[0]}" ]]; then
-    LD_PRELOAD=${DYAD_INSTALL_PREFIX}/lib/dyad_wrapper.so ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/c_cons 10 $DYAD_PATH_CONSUMER
+    LD_PRELOAD=${DYAD_INSTALL_PREFIX}/lib/libdyad_wrapper.so ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/c_cons 10 $DYAD_PATH_CONSUMER
 elif [[ "$mode" == "${valid_modes[1]}" ]]; then
     ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/cpp_cons 10 $DYAD_PATH_CONSUMER
 elif [[ "$mode" == "${valid_modes[2]}" ]]; then
     python3 ${GITHUB_WORKSPACE}/tests/pydyad_spsc/consumer.py $DYAD_PATH_CONSUMER 10 50
 else
-    echo "Invalid test mode: $mode"
+    echo "Invalid cons test mode: $mode"
     exit 1
 fi
--- .github/prod-cons/dyad_producer.sh
+++ .github/prod-cons/dyad_producer.sh
@@ -8,19 +8,19 @@ export DYAD_PATH_PRODUCER=${DYAD_PATH}_producer
 mkdir -p ${DYAD_PATH_PRODUCER}
 echo "Loading DYAD module"
 
-echo flux module load ${DYAD_INSTALL_PREFIX}/lib/libdyad.so  $DYAD_PATH_PRODUCER $DYAD_DTL_MODE
-flux module load ${DYAD_INSTALL_PREFIX}/lib/libdyad.so  $DYAD_PATH_PRODUCER $DYAD_DTL_MODE
+echo flux module load ${DYAD_INSTALL_PREFIX}/lib/dyad.so  $DYAD_PATH_PRODUCER $DYAD_DTL_MODE
+flux module load ${DYAD_INSTALL_PREFIX}/lib/dyad.so  $DYAD_PATH_PRODUCER $DYAD_DTL_MODE
 
 if [[ "$mode" == "${valid_modes[0]}" ]]; then
     echo ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/c_prod 10 $DYAD_PATH_PRODUCER
-    LD_PRELOAD=${DYAD_INSTALL_PREFIX}/lib/dyad_wrapper.so ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/c_prod 10 $DYAD_PATH_PRODUCER
-elif [[ "$mode" == "${test_modes[1]}" ]]; then
+    LD_PRELOAD=${DYAD_INSTALL_PREFIX}/lib/libdyad_wrapper.so ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/c_prod 10 $DYAD_PATH_PRODUCER
+elif [[ "$mode" == "${valid_modes[1]}" ]]; then
     echo ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/cpp_prod 10 $DYAD_PATH_PRODUCER
     ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/cpp_prod 10 $DYAD_PATH_PRODUCER
-elif [[ "$mode" == "${test_modes[2]}" ]]; then
+elif [[ "$mode" == "${valid_modes[2]}" ]]; then
     echo python3 ${GITHUB_WORKSPACE}/tests/pydyad_spsc/consumer.py $DYAD_PATH_PRODUCER 10 50
     python3 ${GITHUB_WORKSPACE}/tests/pydyad_spsc/consumer.py $DYAD_PATH_PRODUCER 10 50
 else
-    echo "Invalid test mode: $mode"
+    echo "Invalid prod test mode: $mode"
     exit 1
 fi
