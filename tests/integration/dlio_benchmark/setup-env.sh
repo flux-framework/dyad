51deb04e5c16195904c503ce342bb58045362ef3
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-01-10 11:47:40 -0800

    use spawn instead of fork

--- tests/integration/dlio_benchmark/setup-env.sh
+++ tests/integration/dlio_benchmark/setup-env.sh
@@ -4,7 +4,7 @@
 export DLIO_WORKLOAD=dyad_unet3d # unet3d dyad_unet3d dyad_unet3d_small
 export NUM_NODES=2
 export PPN=8
-export BROKERS_PER_NODE=2
+export BROKERS_PER_NODE=1
 export GENERATE_DATA="0"
 
 export DYAD_INSTALL_PREFIX=/usr/workspace/haridev/dyad/env/spack/.spack-env/view
@@ -28,7 +28,7 @@ spack env activate -p ${SPACK_ENV}
 source ${PYTHON_ENV}/bin/activate 
 
 # Derived Configurations
-export DYAD_DLIO_RUN_LOG=dyad_${DLIO_WORKLOAD}_${NUM_NODES}_${PPN}.log
+export DYAD_DLIO_RUN_LOG=dyad_${DLIO_WORKLOAD}_${NUM_NODES}_${PPN}_${BROKERS_PER_NODE}.log
 export CONFIG_ARG="--config-dir=${GITHUB_WORKSPACE}/tests/integration/dlio_benchmark/configs"
 #export CONFIG_ARG=""
 
