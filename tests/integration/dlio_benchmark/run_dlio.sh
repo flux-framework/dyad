3451489b4787eea50dd9f086b65abfbb32f6858c
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-01-10 11:47:40 -0800

    use spawn instead of fork

--- tests/integration/dlio_benchmark/run_dlio.sh
+++ tests/integration/dlio_benchmark/run_dlio.sh
@@ -14,20 +14,20 @@ echo List Namespaces
 flux kvs namespace list
 # Setup Local Directories
 echo Setting up local directories
-flux run -N ${NUM_NODES} --tasks-per-node=1 mkdir -p $DYAD_PATH
-flux run -N ${NUM_NODES} --tasks-per-node=1 rm -rf $DYAD_PATH/*
+flux run -N $((NUM_NODES*BROKERS_PER_NODE)) --tasks-per-node=1 mkdir -p $DYAD_PATH
+flux run -N $((NUM_NODES*BROKERS_PER_NODE)) --tasks-per-node=1 rm -rf $DYAD_PATH/*
 
 if [[ "${GENERATE_DATA}" == "1" ]]; then
 # Generate Data for Workload
 echo Generating DLIO Dataset
-flux submit -N ${NUM_NODES} --tasks-per-node=${PPN} dlio_benchmark ${CONFIG_ARG} workload=${DLIO_WORKLOAD} ++workload.dataset.data_folder=${DLIO_DATA_DIR} ++workload.workflow.generate_data=True ++workload.workflow.train=False
+flux submit -N $((NUM_NODES*BROKERS_PER_NODE)) --tasks-per-node=$((PPN/BROKERS_PER_NODE)) dlio_benchmark ${CONFIG_ARG} workload=${DLIO_WORKLOAD} ++workload.dataset.data_folder=${DLIO_DATA_DIR} ++workload.workflow.generate_data=True ++workload.workflow.train=False
 GEN_PID=$(flux job last)
 flux job attach ${GEN_PID}
 fi
 
 # Run Training
 echo Running DLIO Training for ${DLIO_WORKLOAD}
-flux submit -N ${NUM_NODES} -o cpu-affinity=on --tasks-per-node=${PPN} dlio_benchmark ${CONFIG_ARG} workload=${DLIO_WORKLOAD} ++workload.dataset.data_folder=${DLIO_DATA_DIR} ++workload.workflow.generate_data=False ++workload.workflow.train=True
+flux submit -N $((NUM_NODES*BROKERS_PER_NODE)) -o cpu-affinity=on --tasks-per-node=$((PPN/BROKERS_PER_NODE)) dlio_benchmark ${CONFIG_ARG} workload=${DLIO_WORKLOAD} ++workload.dataset.data_folder=${DLIO_DATA_DIR} ++workload.workflow.generate_data=False ++workload.workflow.train=True
 RUN_PID=$(flux job last)
 flux job attach ${RUN_PID} > ${DYAD_DLIO_RUN_LOG} 2>&1
 #cat ${DYAD_DLIO_RUN_LOG}
