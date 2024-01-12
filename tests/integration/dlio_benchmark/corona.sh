28ba5dd82ba5f51423c954da8e3aeb923f5d3dac
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-01-12 15:49:22 -0800

    fixed locality

--- tests/integration/dlio_benchmark/corona.sh
+++ tests/integration/dlio_benchmark/corona.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ./setup-env.sh
-flux alloc -N $NUM_NODES -o per-resource.count=${BROKERS_PER_NODE} --exclusive --broker-opts=--setattr=log-filename=flux.log ./run_dlio.sh
+flux alloc -q pdebug -N $NUM_NODES -o per-resource.count=${BROKERS_PER_NODE} --exclusive --broker-opts=--setattr=log-filename=flux.log ./run_dlio.sh
