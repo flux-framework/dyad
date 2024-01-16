311bfd25b5fd4d4a18edfeb2cb59400a300e0dc8
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-01-15 17:25:08 -0800

    bug fix for locking case

--- tests/integration/dlio_benchmark/corona.sh
+++ tests/integration/dlio_benchmark/corona.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source ./setup-env.sh
+rm *.core flux.log
 flux alloc -q pdebug -N $NUM_NODES -o per-resource.count=${BROKERS_PER_NODE} --exclusive --broker-opts=--setattr=log-filename=flux.log ./run_dlio.sh
