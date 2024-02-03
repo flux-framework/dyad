1fd04c10e9b4a58ef518b2df5c5ed6422fe618b9
Jae-Seung Yeom <yeom2@llnl.gov>
2024-02-02 22:05:00 -0800

    Fix module's arg parsing by replacing the optpase with getopts_long

--- .github/prod-cons/dyad_consumer.sh
+++ .github/prod-cons/dyad_consumer.sh
@@ -2,17 +2,16 @@
 
 this_script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
-source $this_script_dir/prod_cons_argparse.sh
+source ${this_script_dir}/prod_cons_argparse.sh
 
-export DYAD_PATH_CONSUMER=${DYAD_PATH}_consumer
-mkdir -p ${DYAD_PATH_CONSUMER}
+mkdir -m 775 -p ${DYAD_PATH_CONSUMER}
 
 if [[ "$mode" == "${valid_modes[0]}" ]]; then
-    LD_PRELOAD=${DYAD_INSTALL_PREFIX}/lib/libdyad_wrapper.so ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/c_cons 10 $DYAD_PATH_CONSUMER
+    LD_PRELOAD=${DYAD_INSTALL_PREFIX}/lib/libdyad_wrapper.so ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/c_cons 10 ${DYAD_PATH_CONSUMER}
 elif [[ "$mode" == "${valid_modes[1]}" ]]; then
-    ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/cpp_cons 10 $DYAD_PATH_CONSUMER
+    ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/cpp_cons 10 ${DYAD_PATH_CONSUMER}
 elif [[ "$mode" == "${valid_modes[2]}" ]]; then
-    python3 ${GITHUB_WORKSPACE}/tests/pydyad_spsc/consumer.py $DYAD_PATH_CONSUMER 10 50
+    python3 ${GITHUB_WORKSPACE}/tests/pydyad_spsc/consumer.py ${DYAD_PATH_CONSUMER} 10 50
 else
     echo "Invalid cons test mode: $mode"
     exit 1
