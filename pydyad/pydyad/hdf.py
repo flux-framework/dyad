f30904c2d98760b4caab0c8bbf09f38f5640665c
hariharandev1 <hariharandev1@llnl.gov>
2024-03-05 01:56:25 -0800

    changes to support DLIO with MuMMI and DYAD

--- pydyad/pydyad/hdf.py
+++ pydyad/pydyad/hdf.py
@@ -7,35 +7,31 @@ import h5py
 
 class DyadFile(h5py.File):
 
-    def __init__(self, *args, **kwargs, dyad_ctx=None, metadata_wrapper=None):
+    def __init__(self, fname,  mode, file=None, dyad_ctx=None, metadata_wrapper=None):
         # According to H5PY, the first positional argument to File.__init__ is fname
-        self.fname = args[0]
+        self.fname = fname
         if not isinstance(self.fname, Path):
-            self.fname = Path(self.fname)
+            self.fname = Path(fname)
         self.fname = self.fname.expanduser().resolve()
-        self.mode = None
-        if "mode" in kwargs:
-            self.mode = kwargs["mode"]
-        elif len(args) > 1:
-            self.mode = args[1]
-        else:
-            raise NameError("'mode' argument not provided to pydyad.hdf.File constructor")
+        self.m = mode
         if dyad_ctx is None:
             raise NameError("'dyad_ctx' argument not provided to pydyad.hdf.File constructor")
         self.dyad_ctx = dyad_ctx
-        if self.mode in ("r", "rb", "rt"):
+        if self.m in ("r"):
             if (self.dyad_ctx.cons_path is not None and
                     self.dyad_ctx.cons_path in self.fname.parents):
                 if metadata_wrapper:
-                    self.dyad_ctx.consume_w_metadata(str(self.fname), meatadata_wrapper)
+                    self.dyad_ctx.consume_w_metadata(str(self.fname), metadata_wrapper)
                 else:
                     dyad_ctx.consume(str(self.fname))
-        super().__init__(*args, **kwargs)
-
+        if file:
+            super().__init__(file, mode)
+        else:
+            super().__init__(fname, mode)
 
     def close(self):
         super().close()
-        if self.mode in ("w", "wb", "wt"):
+        if self.m in ("w", "r+"):
             if (self.dyad_ctx.prod_path is not None and
                     self.dyad_ctx.prod_path in self.fname.parents):
                 self.dyad_ctx.produce(str(self.fname))
--- tests/integration/dlio_benchmark/setup-env.sh
+++ tests/integration/dlio_benchmark/setup-env.sh
@@ -4,10 +4,10 @@ module load python/3.9.12
 module load openmpi/4.1.2
 
 # Configurations
-export DLIO_WORKLOAD=dyad_resnet50 #dyad_unet3d_large # unet3d_base dyad_unet3d dyad_unet3d_small resnet50_base dyad_resnet50 unet3d_base_large
-export NUM_NODES=32
+export DLIO_WORKLOAD=dyad_mummi #dyad_unet3d_large # unet3d_base dyad_unet3d dyad_unet3d_small resnet50_base dyad_resnet50 unet3d_base_large mummi_base dyad_mummi
+export NUM_NODES=4
 export PPN=8
-export QUEUE=pbatch
+export QUEUE=pdebug
 export TIME=$((60))
 export BROKERS_PER_NODE=1
 export GENERATE_DATA="0"
@@ -21,11 +21,11 @@ export GITHUB_WORKSPACE=/usr/workspace/haridev/dyad
 export SPACK_DIR=/usr/workspace/haridev/spack
 export SPACK_ENV=/usr/workspace/haridev/dyad/env/spack
 export PYTHON_ENV=/usr/workspace/haridev/dyad/env/python
-export DLIO_DATA_DIR=/p/lustre1/iopp/dyad/dlio_benchmark/dyad_resnet50 #  dyad_resnet50 dyad_unet3d_basic
+export DLIO_DATA_DIR=/p/lustre1/iopp/dyad/dlio_benchmark/dyad_mummi #  dyad_resnet50 dyad_unet3d_basic
 #export DLIO_DATA_DIR=/p/lustre2/haridev/dyad/dlio_benchmark/dyad_unet3d_basic #  dyad_resnet50
 
 # DLIO Profiler Configurations
-export DLIO_PROFILER_ENABLE=1
+export DLIO_PROFILER_ENABLE=0
 export DLIO_PROFILER_INC_METADATA=1
 export DLIO_PROFILER_DATA_DIR=${DLIO_DATA_DIR}:${DYAD_PATH}
 export DLIO_PROFILER_LOG_FILE=/usr/workspace/haridev/dyad/tests/integration/dlio_benchmark/profiler/dyad
@@ -35,6 +35,7 @@ export DLIO_PROFILER_LOG_LEVEL=ERROR
 
 export DLIO_PROFILER_BIND_SIGNALS=0
 export MV2_BCAST_HWLOC_TOPOLOGY=0
+export HDF5_USE_FILE_LOCKING=0
 
 #mkdir -p ${DYAD_PATH}
 mkdir -p ${DLIO_PROFILER_LOG_FILE}
