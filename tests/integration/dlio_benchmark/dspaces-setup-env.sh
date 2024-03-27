7edc5a6863c07e08fec2c8a7273345b8cea4d53f
Ian Lumsden <lumsden.ian@gmail.com>
2024-03-27 09:55:17 -0700

    Updates the setup-env and run scripts for DLIO and DataSpaces

--- tests/integration/dlio_benchmark/dspaces-setup-env.sh
+++ tests/integration/dlio_benchmark/dspaces-setup-env.sh
@@ -7,25 +7,26 @@ module load openmpi/4.1.2
 # Configurations
 export DLIO_WORKLOAD=dspaces_mummi #dyad_mummi #dyad_unet3d_large # unet3d_base dyad_unet3d dyad_unet3d_small resnet50_base dyad_resnet50 unet3d_base_large mummi_base dyad_mummi
 export NUM_NODES=64 # 1 for small config, 64 for full config
-export PPN=8 # 1 for small config, 64 for full config
+export PPN=8 # 1 for small config, 8 for full config
 export QUEUE=pbatch
 export TIME=$((60))
 export BROKERS_PER_NODE=1
 export GENERATE_DATA="0"
+export DSPACES_HG_STRING="ofi+verbs"
 
-export DYAD_INSTALL_PREFIX=/usr/workspace/haridev/dyad/env/spack/.spack-env/view
-export GITHUB_WORKSPACE=/usr/workspace/haridev/dyad
-export SPACK_DIR=/usr/workspace/haridev/spack
-export SPACK_ENV=/usr/workspace/haridev/dyad/env/spack
-export PYTHON_ENV=/usr/workspace/haridev/dyad/env/python
-export DLIO_DATA_DIR=/p/lustre1/iopp/dyad/dlio_benchmark/dyad_mummi #  dyad_resnet50 dyad_unet3d_basic
+export GITHUB_WORKSPACE=/g/g90/lumsden1/ws/dyad_sc24_paper_dspaces/dyad
+export SPACK_DIR=/g/g90/lumsden1/ws/spack
+export SPACK_ENV=/g/g90/lumsden1/ws/dyad_sc24_paper_dspaces/baseline_env
+export SPACK_VIEW=$SPACK_ENV/.spack-env/view
+export PYTHON_ENV=/g/g90/lumsden1/ws/dyad_sc24_paper_dspaces/mummi_venv
+export DLIO_DATA_DIR=/p/lustre1/lumsden1/dyad_mummi #  dyad_resnet50 dyad_unet3d_basic
 #export DLIO_DATA_DIR=/p/lustre2/haridev/dyad/dlio_benchmark/dyad_unet3d_basic #  dyad_resnet50
 
 # DLIO Profiler Configurations
 export DLIO_PROFILER_ENABLE=0
 export DLIO_PROFILER_INC_METADATA=1
 export DLIO_PROFILER_DATA_DIR=${DLIO_DATA_DIR}:${DYAD_PATH}
-export DLIO_PROFILER_LOG_FILE=/usr/workspace/haridev/dyad/tests/integration/dlio_benchmark/profiler/dyad
+export DLIO_PROFILER_LOG_FILE=/g/g90/lumsden1/ws/dyad_sc24_paper_dspaces/dyad/tests/integration/dlio_benchmark/profiler/dspaces
 export DLIO_PROFILER_LOG_LEVEL=ERROR
 #export GOTCHA_DEBUG=3
 
@@ -41,13 +42,13 @@ spack env activate -p ${SPACK_ENV}
 source ${PYTHON_ENV}/bin/activate 
 
 # Derived Configurations
-export DYAD_DLIO_RUN_LOG=dyad_${DLIO_WORKLOAD}_${NUM_NODES}_${PPN}_${BROKERS_PER_NODE}.log
+export DSPACES_DLIO_RUN_LOG=dyad_${DLIO_WORKLOAD}_${NUM_NODES}_${PPN}_${BROKERS_PER_NODE}.log
 export CONFIG_ARG="--config-dir=${GITHUB_WORKSPACE}/tests/integration/dlio_benchmark/configs"
 #export CONFIG_ARG=""
 
 # Derived PATHS
-export PATH=${PATH}:${DYAD_INSTALL_PREFIX}/bin:${DYAD_INSTALL_PREFIX}/sbin
-export LD_LIBRARY_PATH=/usr/lib64:${DYAD_INSTALL_PREFIX}/lib:${LD_LIBRARY_PATH}
+export PATH=${PATH}:${SPACK_VIEW}/bin:${SPACK_VIEW}/sbin
+export LD_LIBRARY_PATH=/usr/lib64:${SPACK_VIEW}/lib:${SPACK_VIEW}/lib64:${LD_LIBRARY_PATH}
 export PYTHONPATH=${GITHUB_WORKSPACE}/tests/integration/dlio_benchmark:$PYTHONPATH
 
 unset LUA_PATH
--- tests/integration/dlio_benchmark/dspaces_run_dlio.sh
+++ tests/integration/dlio_benchmark/dspaces_run_dlio.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 source ./dspaces-setup-env.sh 
 
-# TODO startup dspaces_server
+# Startup dspaces_server
+echo "## Config file for DataSpaces server
+ndim = 3
+dims = x
+max_versions = 1
+num_apps = 1" > dataspaces.conf 
+
+flux submit -N $NUM_NODES --cores=$(( NUM_NODES*16 )) \
+    --tasks-per-node=$BROKERS_PER_NODE dspaces_server $DSPACES_HG_STRING
+
+# Wait for DataSpaces's server to create conf.ds
+sleep 1s
+while [ ! -f conf.ds ]; do
+    sleep 1s
+done
+# Give the server enough time to write the contents
+sleep 3s
 
 if [[ "${GENERATE_DATA}" == "1" ]]; then
 # Generate Data for Workload
@@ -13,13 +29,20 @@ echo "Run without Gen data to do training"
 exit
 fi
 
+# Preload data into DataSpaces
+flux run -N $NUM_NODES --tasks-per-node=1 python3 ./dspaces_preloader.py $DLIO_DATA_DIR/train
+
 # Run Training
 echo Running DLIO Training for ${DLIO_WORKLOAD}
-flux submit -N $((NUM_NODES*BROKERS_PER_NODE)) -o cpu-affinity=on --tasks-per-node=$((PPN/BROKERS_PER_NODE)) dlio_benchmark ${CONFIG_ARG} workload=${DLIO_WORKLOAD} ++workload.dataset.data_folder=${DLIO_DATA_DIR} ++workload.workflow.generate_data=False ++workload.workflow.train=True
+flux submit -N $NUM_NODES --cores=$(( NUM_NODES*32 )) -o cpu-affinity=on --tasks-per-node=$PPN \
+    dlio_benchmark ${CONFIG_ARG} workload=${DLIO_WORKLOAD} \
+    ++workload.dataset.data_folder=${DLIO_DATA_DIR} \
+    ++workload.workflow.generate_data=False ++workload.workflow.train=True
 RUN_PID=$(flux job last)
-flux job attach ${RUN_PID} > ${DYAD_DLIO_RUN_LOG} 2>&1
+flux job attach ${RUN_PID} > ${DSPACES_DLIO_RUN_LOG} 2>&1
 #cat ${DYAD_DLIO_RUN_LOG}
-echo "Finished Executing check ${DYAD_DLIO_RUN_LOG} for output"
+echo "Finished Executing check ${DSPACES_DLIO_RUN_LOG} for output"
 
+flux run --ntasks=1 terminator
 
 # TODO run terminate
