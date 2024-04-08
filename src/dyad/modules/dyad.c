d10eff9c11457c3490b3973b4dfa6a37aa8c59c3
Ian Lumsden <lumsden.ian@gmail.com>
2024-04-08 17:37:49 -0400

    Update dyad.c

--- src/dyad/modules/dyad.c
+++ src/dyad/modules/dyad.c
@@ -427,6 +427,7 @@ dyad_rc_t dyad_module_ctx_init (const opt_parse_out_t* opt, flux_t* h)
        DYAD_LOG_STDERR ("DYAD_MOD: DYAD_KVS_NAMESPACE is set to `%s'\n", kvs);
     } else {
         DYAD_LOG_STDERR ("DYAD_MOD: DYAD_KVS_NAMESPACE is not set\n");
+	setenv("DYAD_KVS_NAMESPACE", "dyad_service_dummy", 1);
     }    
     dyad_ctx_init (DYAD_COMM_SEND, h);
     mod_ctx->ctx = dyad_ctx_get ();
