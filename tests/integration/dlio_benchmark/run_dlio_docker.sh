655808ea010708ef3089f10e6874e66ae855346a
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-04-09 16:26:26 -0700

    added docker and run scripts

---
+++ tests/integration/dlio_benchmark/run_dlio_docker.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+source ./setup-env-docker.sh 
+
+# Setup DYAD
+echo Setting up Dyad
+flux kvs namespace create ${DYAD_KVS_NAMESPACE}
+echo "Loading module on brokers"
+flux exec -r all flux module load ${DYAD_INSTALL_PREFIX}/lib/dyad.so --mode="UCX" $DYAD_PATH
+
+echo Loaded Modules
+flux module list
+echo List Namespaces
+flux kvs namespace list
+
+if [[ "${GENERATE_DATA}" == "1" ]]; then
+# Generate Data for Workload
+echo Generating DLIO Dataset
+flux submit --cores=$((NUM_NODES*8)) -N $((NUM_NODES)) --tasks-per-node=$((PPN)) dlio_benchmark ${CONFIG_ARG} workload=${DLIO_WORKLOAD} ++workload.dataset.data_folder=${DLIO_DATA_DIR} ++workload.workflow.generate_data=True ++workload.workflow.train=False
+GEN_PID=$(flux job last)
+flux job attach ${GEN_PID}
+echo "Run without Gen data to do training"
+exit
+fi
+
+# Run Training
+echo Running DLIO Training for ${DLIO_WORKLOAD}
+flux submit --cores=$((NUM_NODES*8)) -N $((NUM_NODES)) --tasks-per-node=$((PPN)) dlio_benchmark ${CONFIG_ARG} workload=${DLIO_WORKLOAD} ++workload.dataset.data_folder=${DLIO_DATA_DIR} ++workload.workflow.generate_data=False ++workload.workflow.train=True
+RUN_PID=$(flux job last)
+flux job attach ${RUN_PID} > ${DYAD_DLIO_RUN_LOG} 2>&1
+#cat ${DYAD_DLIO_RUN_LOG}
+echo "Finished Executing check ${DYAD_DLIO_RUN_LOG} for output"
+
+
+# Clean DYAD
+echo Cleaning DYAD
+flux kvs namespace remove ${DYAD_KVS_NAMESPACE}
+flux exec -r all flux module unload dyad
+
---
+++ tests/integration/dlio_benchmark/setup-env-docker.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+module load python/3.9.12
+module load openmpi/4.1.2
+
+# Configurations
+export DLIO_WORKLOAD=dyad_unet3d_small
+export NUM_NODES=1
+export PPN=1
+export GENERATE_DATA="0"
+
+export DYAD_INSTALL_PREFIX=/mount/shared/install
+export DYAD_KVS_NAMESPACE=dyad
+export DYAD_DTL_MODE=UCX
+export DYAD_PATH="/mount/ssd"
+export GITHUB_WORKSPACE=/mount/shared/code
+export SPACK_DIR=/mount/shared/spack
+export DLIO_DATA_DIR=/mount/pfs
+
+
+# DLIO Profiler Configurations
+export DLIO_PROFILER_ENABLE=0
+
+# Derived Configurations
+export DYAD_DLIO_RUN_LOG=dyad_${DLIO_WORKLOAD}_${NUM_NODES}_${PPN}.log
+export CONFIG_ARG="--config-dir=${GITHUB_WORKSPACE}/tests/integration/dlio_benchmark/configs"
+
+
+# Derived PATHS
+export PATH=${PATH}:${DYAD_INSTALL_PREFIX}/bin:${DYAD_INSTALL_PREFIX}/sbin
+export LD_LIBRARY_PATH=/usr/lib64:${DYAD_INSTALL_PREFIX}/lib:${LD_LIBRARY_PATH}
+export PYTHONPATH=${GITHUB_WORKSPACE}/tests/integration/dlio_benchmark:${GITHUB_WORKSPACE}/pydyad:$PYTHONPATH
+
+
+mkdir -p ${DLIO_DATA_DIR} ${DYAD_PATH}
+unset LUA_PATH
+unset LUA_CPATH
