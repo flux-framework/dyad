d0640dea7fb83ae945505a22c07ddb725d872579
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-04-09 17:45:33 -0700

    fixing scripts

--- tests/integration/dlio_benchmark/docker.sh
+++ tests/integration/dlio_benchmark/docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 GENERATE_DATA=$1
-source ./setup-env-demo.sh $GENERATE_DATA
+source ./setup-env-docker.sh $GENERATE_DATA
 flux start --test-size=$NUM_NODES ./run_dlio_docker.sh $GENERATE_DATA
