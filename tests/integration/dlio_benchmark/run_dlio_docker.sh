1e596f80d39248867c053482d9d2e2cd8eea652d
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-04-09 18:10:38 -0700

    fix scripts

--- tests/integration/dlio_benchmark/run_dlio_docker.sh
+++ tests/integration/dlio_benchmark/run_dlio_docker.sh
@@ -16,18 +16,14 @@ flux kvs namespace list
 if [[ "${GENERATE_DATA}" == "1" ]]; then
 # Generate Data for Workload
 echo Generating DLIO Dataset
-flux submit --cores=$((NUM_NODES*8)) -N $((NUM_NODES)) --tasks-per-node=$((PPN)) dlio_benchmark ${CONFIG_ARG} workload=${DLIO_WORKLOAD} ++workload.dataset.data_folder=${DLIO_DATA_DIR} ++workload.workflow.generate_data=True ++workload.workflow.train=False
-GEN_PID=$(flux job last)
-flux job attach ${GEN_PID}
+flux run --cores=$((NUM_NODES*8)) -N $((NUM_NODES)) --tasks-per-node=$((PPN)) dlio_benchmark ${CONFIG_ARG} workload=${DLIO_WORKLOAD} ++workload.dataset.data_folder=${DLIO_DATA_DIR} ++workload.workflow.generate_data=True ++workload.workflow.train=False
 echo "Run without Gen data to do training"
 exit
 fi
 
 # Run Training
 echo Running DLIO Training for ${DLIO_WORKLOAD}
-flux submit --cores=$((NUM_NODES*8)) -N $((NUM_NODES)) --tasks-per-node=$((PPN)) dlio_benchmark ${CONFIG_ARG} workload=${DLIO_WORKLOAD} ++workload.dataset.data_folder=${DLIO_DATA_DIR} ++workload.workflow.generate_data=False ++workload.workflow.train=True
-RUN_PID=$(flux job last)
-flux job attach ${RUN_PID} > ${DYAD_DLIO_RUN_LOG} 2>&1
+flux run --cores=$((NUM_NODES*8)) -N $((NUM_NODES)) --tasks-per-node=$((PPN)) dlio_benchmark ${CONFIG_ARG} workload=${DLIO_WORKLOAD} ++workload.dataset.data_folder=${DLIO_DATA_DIR} ++workload.workflow.generate_data=False ++workload.workflow.train=True
 #cat ${DYAD_DLIO_RUN_LOG}
 echo "Finished Executing check ${DYAD_DLIO_RUN_LOG} for output"
 
