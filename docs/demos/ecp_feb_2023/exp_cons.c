b15c89fe0f5a2075134a4d1c364d2a42a3585d84
Ian Lumsden <lumsden.ian@gmail.com>
2024-07-03 11:41:07 -0700

    Adds test for explicit C API

---
+++ docs/demos/ecp_feb_2023/exp_cons.c
@@ -0,0 +1,100 @@
+#include "generation.h" /* Defines the following:
+                         *  - parse_cmd_line
+                         *  - ALLOC_VAL_BUF
+                         *  - vals_are_valid
+                         *  - VAL_BUF_SIZE
+                         *  - NUM_VALS
+                         */
+#include <dyad/c_api/dyad_c.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * Transfer the specified file with DYAD and read it
+ *
+ * @param[in] full_path Path to the file to read
+ * @param[in] seed      The "seed value" (i.e., number from the for-loop in main)
+ *                      for the file
+ * @param[out] val_buf  Array in which to store the contents of the file
+ *
+ * @return 0 if no errors occured. -1 otherwise.
+ */
+int consume_file (dyad_ctx_t* ctx, char* full_path, int32_t seed, int32_t* val_buf)
+{
+    // Open the file or abort if not possible
+    int fd = dyad_c_open (ctx, full_path, O_RDONLY);
+    // Print an error if the file couldn't be opened
+    if (fd < 0) {
+        fprintf (stderr, "Cannot open file: %s\n", full_path);
+        printf ("File %ld: Cannot open\n", (long)seed);
+        return -1;
+    }
+    // Read the file or abort if not possible
+    ssize_t items_read = read (fd, val_buf, sizeof (int32_t) * NUM_VALS);
+    dyad_c_close (ctx, fd);
+    // Print an error if the number of items read is not what was expected
+    if (items_read != NUM_VALS * sizeof (int32_t)) {
+        fprintf (stderr, "Could not read the full file (%s)\n", full_path);
+        printf ("File %ld: Cannot read\n", (long)seed);
+        return -1;
+    }
+    return 0;
+}
+
+int main (int argc, char** argv)
+{
+    // Parse command-line arguments
+    int32_t num_transfers;
+    char* fpath;
+    int rc = parse_cmd_line (argc, argv, &num_transfers, &fpath);
+    // If an error occured during command-line parsing,
+    // abort the consumer
+    if (rc != 0) {
+        return rc;
+    }
+    dyad_ctx_t* ctx;
+    rc = dyad_c_init (&ctx);
+    if (rc < 0) {
+        return rc;
+    }
+    // Largest number of digits for a int32_t when converted to string
+    const size_t max_digits = 10;
+    // First 4 is for "data"
+    // Second 4 is for ".txt"
+    size_t path_len = strlen (fpath) + 4 + max_digits + 4;
+    // Allocate a buffer for the file path
+    char* full_path = malloc (path_len + 1);
+    // Allocate a buffer for the data read from the file
+    int32_t* val_buf = ALLOC_VAL_BUF ();
+    int num_chars;
+    for (int32_t seed = 0; seed < num_transfers; seed++) {
+        // Clear full_path and val_buf to be safe
+        memset (full_path, '\0', path_len + 1);
+        memset (val_buf, 0, VAL_BUF_SIZE);
+        // Generate the file name
+        num_chars = snprintf (full_path, path_len + 1, "%s/data%ld.txt", fpath, (long)seed);
+        // If the file name could not be generated, print an error
+        // and abort
+        if (num_chars < 0) {
+            fprintf (stderr, "Could not generate file name for transfer %ld\n", (long)seed);
+            printf ("File %ld: Bad Name\n", (long)seed);
+            continue;
+        }
+        /********************************************
+         *       Perform DYAD Data Transfer!!!      *
+         ********************************************/
+        rc = consume_file (ctx, full_path, seed, val_buf);
+        if (rc != 0) {
+            continue;
+        }
+        // Validate the content of the file
+        if (vals_are_valid (seed, val_buf)) {
+            printf ("File %ld: OK\n", (long)seed);
+        } else {
+            printf ("File %ld: BAD\n", (long)seed);
+        }
+    }
+    free (val_buf);
+    free (full_path);
+    dyad_c_finalize (&ctx);
+}
