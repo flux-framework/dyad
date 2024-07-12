22280d1c7ce6d7db5df1ea06a07ced77779ca956
Ian Lumsden <lumsden.ian@gmail.com>
2024-07-11 17:30:15 -0700

    Reworks Caliper annotations to use custom code instead of just macros

--- src/dyad/common/dyad_profiler.h
+++ src/dyad/common/dyad_profiler.h
@@ -11,11 +11,14 @@
 #endif
 #ifdef DYAD_PROFILER_CALIPER
 #include <caliper/cali.h>
-#endif
 
-// Number of updates that can be added to a single profiling region.
-// Currently here because it's required to have for Caliper support.
-#define DYAD_PROFILER_NUM_UPDATES 10
+#include <string>
+#include <vector>
+
+#ifdef __cplusplus
+#include <caliiper/Annotation.h>
+#endif
+#endif
 
 // clang-format off
 #ifdef __cplusplus
@@ -32,22 +35,118 @@
   #define DYAD_CPP_REGION_END(name) DYAD_NOOP_MACRO
   #define DYAD_CPP_REGION_UPDATE(name, key, value) DYAD_NOOP_MACRO
 #elif defined(DYAD_PROFILER_CALIPER)  // CALIPER
-  #define DYAD_CPP_FUNCTION() CALI_CXX_MARK_FUNCTION;
-  #define DYAD_CPP_FUNCTION_UPDATE(key, value) DYAD_CPP_REGION_UPDATE(__func__, (key), (value));
-  #define DYAD_CPP_REGION_START(name) CALI_MARK_BEGIN (name);
-  #define DYAD_CPP_REGION_END(name) CALI_MARK_END (name);
-  // Cannot currently implement this with Caliper because one of the following must be true
-  //   * Caliper annotation must begin before region begins and end after region ends
-  //   * Caliper annotation must be created with CALI_ATTR_UNALIGNED, and Caliper annotation must
-  //     begin after region begins and end after region ends
-  // The first bullet is incompatible with how DFTracer/DLIO Profiler works. The second bullet
-  // cannot be done with Caliper's C++ RAII wrappers.
-  //
-  // I've talked with David Boehme about this, and he may add an API to enable this.
-  // But, until then, this macro cannot be defined.
-  //
-  // TODO implement if/when David adds an API that is compatible with these macros
-  #define DYAD_CPP_REGION_UPDATE(name, key, value) DYAD_NOOP_MACRO
+namespace dyad {
+
+class CaliperEntryList
+{
+  public:
+
+    template <typename T>
+    void update(const std::string& key, T& value)
+    {
+      update(key.c_str(), value);
+    }
+    
+    template <typename T>
+    void update(const char* key, T& value)
+    {
+      cali::Annotation* entry = new cali::Annotation(key, CALI_ATTR_SKIP_EVENTS | CALI_ATTR_UNALIGNED);
+      entry->begin(value);
+      m_entries.push_back(entry);
+    }
+    
+  protected:
+
+    std::vector<cali::Annotation*> m_entries;
+};
+
+class CaliperFunction : public CaliperEntryList
+{
+  public:
+
+    CaliperFunction(const char* name)
+      : m_cali_func(new cali::Function(name))
+    {
+    }
+    
+    CaliperFunction(const std::string& name)
+      : CaliperFunction(name.c_str())
+    {
+    }
+    
+    ~CaliperFunction()
+    {
+      end();
+    }
+    
+    void end()
+    {
+      if (m_cali_func != NULL) {
+        delete m_cali_func;
+      }
+      for (size_t i = 0; i < m_entries; i++) {
+        if (m_entries[i] != NULL) {
+          m_entries[i]->end();
+          delete m_entries[i];
+          m_entries[i] = NULL;
+        }
+      }
+      m_entries.clear();
+    }
+    
+  private:
+
+    cali::Function* m_cali_func;
+
+};
+
+class CaliperRegion : public CaliperEntryList
+{
+  public:
+
+    CaliperRegion(const char* name)
+      : m_cali_region(new cali::ScopeAnnotation(name))
+    {
+    }
+    
+    CaliperRegion(const std::string& name)
+      : CaliperRegion(name.c_str())
+    {
+    }
+    
+    ~CaliperRegion()
+    {
+      end();
+    }
+    
+    void end()
+    {
+      if (m_cali_region != NULL) {
+        delete m_cali_func;
+      }
+      for (size_t i = 0; i < m_entries; i++) {
+        if (m_entries[i] != NULL) {
+          m_entries[i]->end();
+          delete m_entries[i];
+          m_entries[i] = NULL;
+        }
+      }
+      m_entries.clear();
+    }
+    
+  private:
+
+    cali::ScopeAnnotation* m_cali_region;
+
+};
+
+}  // namespace dyad
+
+  #define DYAD_CPP_FUNCTION() dyad::CaliperFunction dyad_cali_fn_region{__func__}
+  #define DYAD_CPP_FUNCTION_UPDATE(key, value) dyad_cali_fn_region.update((key), (value))
+  #define DYAD_CPP_REGION_START(name) dyad::CaliperRegion* dyad_cali_region_##name = new dyad::CaliperRegion(#name)
+  #define DYAD_CPP_REGION_END(name) delete dyad_cali_region_##name
+  #define DYAD_CPP_REGION_UPDATE(name, key, value) dyad_cali_region_##name->update((key), (value))
 #elif defined(DYAD_PROFILER_DLIO_PROFILER)  // DLIO_PROFILER
   #define DYAD_CPP_FUNCTION() DLIO_PROFILER_CPP_FUNCTION ()
   #define DYAD_CPP_FUNCTION_UPDATE(key, value) DLIO_PROFILER_CPP_FUNCTION_UPDATE ((key), (value))
@@ -81,84 +180,102 @@ extern "C" {
   #define DYAD_C_FUNCTION_UPDATE_INT(key, value) DYAD_NOOP_MACRO
   #define DYAD_C_FUNCTION_UPDATE_STR(key, value) DYAD_NOOP_MACRO
 #elif defined(DYAD_PROFILER_CALIPER)  // CALIPER
+  struct dyad_profiler_caliper_region_entries {
+    cali_id_t* entries;
+    size_t size;
+    size_t capacity;
+  };
+
+  typedef struct dyad_profiler_caliper_region_entries dyad_profiler_caliper_region_entries_t;
+  
+  dyad_profiler_caliper_region_entries_t* dyad_profiler_caliper_region_start(const char* region_name)
+  {
+    dyad_profiler_caliper_region_entries_t* entry_list = (dyad_profiler_caliper_region_entries_t*) malloc(sizeof(struct dyad_profiler_caliper_region_entries));
+    if (entry_list == NULL) {
+      return NULL;
+    }
+    entry_list->capacity = 10;
+    entry_list->size = 0;
+    entry_list->entries = (cali_id_t*) malloc(entry_list->capacity * sizeof(cali_id_t));
+    if (entry_list->entries == NULL) {
+      free(entry_list);
+      entry_list = NULL;
+    }
+    CALI_MARK_BEGIN(region_name);
+    return entry_list;
+  }
+  
+  void dyad_profiler_caliper_region_end(dyad_profiler_caliper_region_entries_t** entry_list, const char* region_name)
+  {
+    CALI_MARK_END(region_name);
+    if (entry_list != NULL && *entry_list != NULL) {
+      if ((*entry_list)->entries != NULL) {
+        for (size_t i = 0; i < (*entry_list)->size; i++) {
+          cali_end((*entry_list)->entries[i]);
+        }
+        free((*entry_list)->entries);
+      }
+      free(*entry_list);
+      *entry_list = NULL;
+    }
+  }
+  
+  void dyad_profiler_caliper_region_update_int(dyad_profiler_caliper_region_entries_t* entry_list, const char* key, int value)
+  {
+    if (entry_list != NULL) {
+      if (entry_list->size >= entry_list->capacity) {
+        size_t new_capacity = 2 * entry_list->capacity;
+        cali_id_t* realloced_cali_id_buf = (cali_id_t*) realloc(entry_list->entries, new_capacity * sizeof(cali_id_t));
+        if (realloced_cali_id_buf != NULL) {
+          entry_list->entries = realloced_cali_id_buf;
+          entry_list->capacity = new_capacity;
+        }
+      }
+      if (entry_list->size < entry_list->capacity) {
+        cali_id_t cali_attr_id = cali_create_attribute(key, CALI_TYPE_INT, CALI_ATTR_SKIP_EVENTS | CALI_ATTR_UNALIGNED);
+        cali_begin_int(cali_attr_id, value);
+        entry_list->entries[entry_list->size] = cali_id_t;
+        entry_list->size += 1;
+      }
+    }
+  }
+  
+  void dyad_profiler_caliper_region_update_string(dyad_profiler_caliper_region_entries_t* entry_list, const char* key, char* value)
+  {
+    if (entry_list != NULL) {
+      if (entry_list->size >= entry_list->capacity) {
+        size_t new_capacity = 2 * entry_list->capacity;
+        cali_id_t* realloced_cali_id_buf = (cali_id_t*) realloc(entry_list->entries, new_capacity * sizeof(cali_id_t));
+        if (realloced_cali_id_buf != NULL) {
+          entry_list->entries = realloced_cali_id_buf;
+          entry_list->capacity = new_capacity;
+        }
+      }
+      if (entry_list->size < entry_list->capacity) {
+        cali_id_t cali_attr_id = cali_create_attribute(key, CALI_TYPE_STRING, CALI_ATTR_SKIP_EVENTS | CALI_ATTR_UNALIGNED);
+        cali_begin_string(cali_attr_id, value);
+        entry_list->entries[entry_list->size] = cali_id_t;
+        entry_list->size += 1;
+      }
+    }
+  }
+                                      
   #define DYAD_C_REGION_START(name) \
-      size_t cali_updated_entries_##name##_capacity = DYAD_PROFILER_NUM_UPDATES; \
-      cali_id_t* cali_updated_entries_##name = (cali_id_t*) malloc(cali_updated_entries_##name##_capacity * sizeof(cali_id_t)); \
-      size_t num_current_cali_updates_##name = 0; \
-      CALI_MARK_BEGIN ((name));
+      dyad_profiler_caliper_region_entries_t* dyad_caliper_entries_for_region_##name = dyad_profiler_caliper_region_start(#name);
   #define DYAD_C_REGION_END(name) \
-      CALI_MARK_END ((name)); \
-      for (size_t cali_update_##name##_it = 0; cali_update_##name##_it < num_current_cali_updates_##name; cali_update_##name##_it += 1) { \
-        cali_end(cali_updated_entries_##name[cali_update_##name##_it]); \
-      } \
-      free(cali_updated_entries_##name);
+      dyad_profiler_caliper_region_end(&dyad_caliper_entries_for_region_##name, #name);
   #define DYAD_C_REGION_UPDATE_INT(name, key, value) \
-      if (num_current_cali_updates_##name >= cali_updated_entries_##name##_capacity) { \
-        cali_id_t* realloced_buffer_for_region_##name = (cali_id_t*) realloc(cali_updated_entries_##name, 2 * cali_updated_entries_##name##_capacity * sizeof(cali_id_t)); \
-        if (realloced_buffer_for_region_##name != NULL) { \
-          cali_updated_entries_##name = realloced_buffer_for_region_##name; \
-          cali_updated_entries_##name##_capacity *= 2; \
-        } \
-      } \
-      if (num_current_cali_updates_##name < cali_updated_entries_##name##_capacity) { \
-        cali_id_t cali_attr_region_##name##_user_key = cali_create_attribute((key), CALI_TYPE_INT, CALI_ATTR_SKIP_EVENTS | CALI_ATTR_UNALIGNED); \
-        cali_begin_int(cali_attr_region_##name##_user_key, (value)); \
-        cali_updated_entries_##name[num_current_cali_updates_##name] = cali_attr_region_##name##_user_key; \
-        num_current_cali_updates_##name += 1; \
-      }
+      dyad_profiler_caliper_region_update_int(dyad_caliper_entries_for_region_##name, key, value);
   #define DYAD_C_REGION_UPDATE_STR(name, key, value) \
-      if (num_current_cali_updates_##name >= cali_updated_entires_##name##_capacity) { \
-        cali_id_t* realloced_buffer_for_region_##name = (cali_id_t*) realloc(cali_updated_entries_##name, 2 * cali_updated_entries_##name##_capacity * sizeof(cali_id_t)); \
-        if (realloced_buffer_for_region_##name != NULL) { \
-          cali_updated_entries_##name = realloced_buffer_for_region_##name; \
-          cali_updated_entries_##name##_capacity *= 2; \
-        } \
-      } \
-      if (num_current_cali_updates_##name < cali_updated_entries_##name##_capacity) { \
-        cali_id_t cali_attr_region_##name##_user_key = cali_create_attribute((key), CALI_TYPE_STRING, CALI_ATTR_SKIP_EVENTS | CALI_ATTR_UNALIGNED); \
-        cali_begin_string(cali_attr_region_##name##_user_key, (value)); \
-        cali_updated_entries_##name[num_current_cali_updates_##name] = cali_attr_region_##name##_user_key; \
-        num_current_cali_updates_##name += 1; \
-      }
+      dyad_profiler_caliper_region_update_string(dyad_caliper_entries_for_region_##name, key, value);
   #define DYAD_C_FUNCTION_START() \
-      size_t cali_updated_entries_fn_capacity = DYAD_PROFILER_NUM_UPDATES; \
-      cali_id_t* cali_updated_entries_fn = (cali_id_t*) malloc(cali_updated_entries_fn_capacity * sizeof(cali_id_t)); \
-      size_t num_current_cali_updates_fn = 0; \
-      CALI_MARK_FUNCTION_BEGIN;
+      dyad_profiler_caliper_region_entries_t* dyad_caliper_entries_fn = dyad_profiler_caliper_region_start(__func__);
   #define DYAD_C_FUNCTION_END() \
-      CALI_MARK_FUNCTION_END; \
-      for (size_t cali_update_fn_it = 0; cali_update_fn_it < num_current_cali_updates_fn; cali_update_fn_it += 1) { \
-        cali_end(cali_updated_entries_fn[cali_update_fn_it]); \
-      } \
-      free(cali_updated_entries_fn);
+      dyad_profiler_caliper_region_end(&dyad_caliper_entries_fn, __func__);
   #define DYAD_C_FUNCTION_UPDATE_INT(key, value) \
-      if (num_current_cali_updates_fn >= cali_updated_entries_fn_capacity) { \
-        cali_id_t* realloced_buffer_for_region_fn = (cali_id_t*) realloc(cali_updated_entries_fn, 2 * cali_updated_entries_fn_capacity * sizeof(cali_id_t)); \
-        if (realloced_buffer_for_region_fn != NULL) { \
-          cali_updated_entries_fn = realloced_buffer_for_region_fn; \
-          cali_updated_entries_fn_capacity *= 2; \
-        } \
-      } \
-      if (num_current_cali_updates_fn < cali_updated_entries_fn_capacity) { \
-        cali_id_t cali_attr_region_fn_user_key = cali_create_attribute((key), CALI_TYPE_INT, CALI_ATTR_SKIP_EVENTS | CALI_ATTR_UNALIGNED); \
-        cali_begin_int(cali_attr_region_fn_user_key, (value)); \
-        cali_updated_entries_fn[num_current_cali_updates_fn] = cali_attr_region_fn_user_key; \
-        num_current_cali_updates_fn += 1; \
-      } 
+      dyad_profiler_caliper_region_update_int(dyad_caliper_entries_fn, key, value);
   #define DYAD_C_FUNCTION_UPDATE_STR(key, value) \
-      if (num_current_cali_updates_fn >= cali_updated_entries_fn_capacity) { \
-        cali_id_t* realloced_buffer_for_region_fn = (cali_id_t*) realloc(cali_updated_entries_fn, 2 * cali_updated_entries_fn_capacity * sizeof(cali_id_t)); \
-        if (realloced_buffer_for_region_fn != NULL) { \
-          cali_updated_entries_fn = realloced_buffer_for_region_fn; \
-          cali_updated_entries_fn_capacity *= 2; \
-        } \
-      } \
-      if (num_current_cali_updates_fn < cali_updated_entries_fn_capacity) { \
-        cali_id_t cali_attr_region_fn_user_key = cali_create_attribute((key), CALI_TYPE_STRING, CALI_ATTR_SKIP_EVENTS | CALI_ATTR_UNALIGNED); \
-        cali_begin_string(cali_attr_region_fn_user_key, (value)); \
-        cali_updated_entries_fn[num_current_cali_updates_fn] = cali_attr_region_fn_user_key; \
-        num_current_cali_updates_fn += 1; \
-      }
+      dyad_profiler_caliper_region_update_string(dyad_caliper_entries_fn, key, value);
 #elif defined(DYAD_PROFILER_DLIO_PROFILER)  // DLIO_PROFILER
   #define DYAD_C_REGION_START(name) DLIO_PROFILER_C_REGION_START ((name))
   #define DYAD_C_REGION_END(name) DLIO_PROFILER_C_REGION_END ((name))
