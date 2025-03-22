bfd8966802bba23eabc0f38f1c5d7d1e407782f5
Jae-Seung Yeom <JaeseungYeom@users.noreply.github.com>
2025-03-22 15:32:48 -0700

    Allow lib64 as the dyad library installation path in test scripts (#132)

--- .github/prod-cons/dyad_consumer.sh
+++ .github/prod-cons/dyad_consumer.sh
@@ -6,8 +6,17 @@ source ${this_script_dir}/prod_cons_argparse.sh
 
 mkdir -m 775 -p ${DYAD_PATH_CONSUMER}
 
+if [ -d ${DYAD_INSTALL_PREFIX}/lib64 ] ; then
+    DYAD_INSTALL_LIBDIR=${DYAD_INSTALL_PREFIX}/lib64
+elif [ -d ${DYAD_INSTALL_PREFIX}/lib ] ; then
+    DYAD_INSTALL_LIBDIR=${DYAD_INSTALL_PREFIX}/lib
+else
+    echo "Cannot find DYAD LIB DIR"
+    exit 1
+fi
+
 if [[ "$mode" == "${valid_modes[0]}" ]]; then
-    LD_PRELOAD=${DYAD_INSTALL_PREFIX}/lib/libdyad_wrapper.so ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/c_cons 10 ${DYAD_PATH_CONSUMER}
+    LD_PRELOAD=${DYAD_INSTALL_LIBDIR}/libdyad_wrapper.so ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/c_cons 10 ${DYAD_PATH_CONSUMER}
 elif [[ "$mode" == "${valid_modes[1]}" ]]; then
     ${GITHUB_WORKSPACE}/docs/demos/ecp_feb_2023/cpp_cons 10 ${DYAD_PATH_CONSUMER}
 elif [[ "$mode" == "${valid_modes[2]}" ]]; then
